5ca96b0026af828bfa2f16e269795b7b54646499
Liu Ji Yin <you@xh2453753009@163.com>
2020-09-22 13:17:36 +0800

    third commit

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,9 +1,12 @@
 function createElement(tagName,attributes,...children) {
  let e  = document.createElement(tagName);
- for(let p in attributes) {
+    for(let p in attributes) {
+        e.setAttribute(p,attributes[p]);
+    }
+    for(let child of children) { 
+        e.appendChild(child);
+    }
 
-    
- }
-
+    return e;
 
 }
